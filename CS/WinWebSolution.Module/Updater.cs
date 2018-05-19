7f2da3343e1006037990b5ba0bd24d1b4d881283
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/WinWebSolution.Module/Updater.cs
@@ -0,0 +1,19 @@
+using System;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+
+namespace WinWebSolution.Module {
+    public class Updater : ModuleUpdater {
+        public Updater(Session session, Version currentDBVersion) : base(session, currentDBVersion) { }
+        public override void UpdateDatabaseAfterUpdateSchema() {
+            base.UpdateDatabaseAfterUpdateSchema();
+            TestAggregatedPropertiesObject obj = Session.FindObject<TestAggregatedPropertiesObject>(CriteriaOperator.Parse("Name=?", "TestAggregatedPropertiesObject"));
+            if (obj == null) {
+                obj = new TestAggregatedPropertiesObject(Session);
+                obj.Name = "TestAggregatedPropertiesObject";
+                obj.Save();
+            }
+        }
+    }
+}
---
+++ VB/VB/WinWebSolution.Module/Updater.vb
@@ -0,0 +1,23 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.Xpo
+Imports DevExpress.Data.Filtering
+
+Namespace WinWebSolution.Module
+	Public Class Updater
+		Inherits ModuleUpdater
+		Public Sub New(ByVal session As Session, ByVal currentDBVersion As Version)
+			MyBase.New(session, currentDBVersion)
+		End Sub
+		Public Overrides Sub UpdateDatabaseAfterUpdateSchema()
+			MyBase.UpdateDatabaseAfterUpdateSchema()
+			Dim obj As TestAggregatedPropertiesObject = Session.FindObject(Of TestAggregatedPropertiesObject)(CriteriaOperator.Parse("Name=?", "TestAggregatedPropertiesObject"))
+			If obj Is Nothing Then
+				obj = New TestAggregatedPropertiesObject(Session)
+				obj.Name = "TestAggregatedPropertiesObject"
+				obj.Save()
+			End If
+		End Sub
+	End Class
+End Namespace
